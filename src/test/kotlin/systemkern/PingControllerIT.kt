a443234212a7d694365b9f2a35e139a9fabcd8f4
Alper Tekinalp <alper@localhost.localdomain>
2019-01-03 13:11:30 +0300

    Simplify the project architecture
    Fixes: OSS-5

--- src/test/kotlin/systemkern/PingControllerIT.kt
+++ src/test/kotlin/systemkern/PingControllerIT.kt
@@ -14,6 +14,7 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+
 @RunWith(SpringRunner::class)
 @ActiveProfiles("integration-test")
 @SpringBootTest(webEnvironment = RANDOM_PORT, classes = [CliEntryPoint::class])
